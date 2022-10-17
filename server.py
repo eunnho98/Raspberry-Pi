6cdf3f2eae10a678044d33c88649e26095b2f28a
taemin4u <taemin4u@khu.ac.kr>
2022-10-18 06:32:24 +0900

    라즈베리파이 소켓 통신

---
+++ server.py
@@ -0,0 +1,48 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import _thread
+import socket
+
+end = 0
+print("안녕하세요")
+
+
+def threaded(client_socket, addr):
+    print('Connected by: ', addr[0], ':', addr[1])
+
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                print('Disconnected by ' + addr[0], ':', addr[1])
+                break
+
+            print('Received from ' + addr[0], ':', addr[1], data.decode())
+
+            client_socket.send(data)
+        except ConnectionResetError as e:
+            print("Disconnected by", addr[0], ':', addr[1])
+            print(f"e: {e}")
+
+
+ip = '127.0.0.1'
+port = 8080
+
+# 소켓 초기화
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 소켓 에러처리
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((ip, port))
+
+server_socket.listen()
+
+print('server start')
+
+while True:
+    if end == 1:
+        break
+print("wait")
+cs, addr = server_socket.accept()
+_thread.start_new_thread(threaded, (cs, addr))
