12bfb60b5b1b67bacaa8d2761e38673501839b06
taemin4u <taemin4u@khu.ac.kr>
2022-11-30 03:05:55 +0900

    부저음 구현

--- test/pi.py
+++ test/pi.py
@@ -99,3 +99,12 @@ tsend.join()
 client_socket.close()
 server_socket.close()
 
+# if __name__ == '__main__':
+#   # 소켓을 이용해 로컬 IP 찾기
+# 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 	s.connect(("8.8.8.8", 80))
+# 	ip = s.getsockname()[0]
+# 	print(s.getsockname()[0])
+# 	s.close()
+
+# https://surprisecomputer.tistory.com/25 부팅 시 프로그램 자동 실행
